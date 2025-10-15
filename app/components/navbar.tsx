e946d6134a75acccec2353c9083515dfcb869af3
SpiderFunction <spiderfunction@gmail.com>
2025-10-15 17:11:13 +0800

    Spider Function trademark added

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -10,7 +10,7 @@ type Props = {
 const Navbar = ({ addedClass }: Props) => {
   return (
     <nav className={`sticky top-0 w-full z-50 ${addedClass}`}>
-      <div className='flex flex-row justify-between items-center px-12 py-2 border-b space-x-2 uppercase tracking-wider text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
+      <div className='flex flex-row justify-between items-center px-8 py-2 border-b space-x-2 uppercase tracking-wider text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
         <span></span>
         <Link href={'/'} className='hover:text-primary transition-all duration-200'>
           <span className=''>Anthony </span>
@@ -18,17 +18,22 @@ const Navbar = ({ addedClass }: Props) => {
         </Link>
         <ThemeToggle />
       </div>
-      <ul className="flex flex-row space-x-8 justify-center items-center px-4 py-2 md:text-lg">
-        <li>
-          <NavLink href="/about">About</NavLink>
-        </li>
-        <li>
-          <NavLink href="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink href="/resume">Resume</NavLink>
-        </li>
-      </ul>
+      <div className='lg:hidden text-xs uppercase tracking-wider text-center py-2'>Made with love by <Link href={'https://spiderfunction.com'} className='text-primary'>Spider Function</Link></div>
+      <div className='flex flex-row justify-between items-center w-full px-8'>
+        <div className='hidden lg:block w-[33%]'></div>
+        <ul className="w-full lg:w-[33%] flex flex-row space-x-8 justify-center items-center px-4 py-2 md:text-lg">
+          <li>
+            <NavLink href="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink href="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink href="/resume">Resume</NavLink>
+          </li>
+        </ul>
+        <div className='hidden lg:block w-[33%] text-sm uppercase tracking-wider text-right'>Made with love by <Link href={'https://spiderfunction.com'} className='text-primary'>Spider Function</Link></div>
+      </div>
     </nav>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
     <>
       <div className="relative h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("/hero-bg.png")' }}>
         <nav className='absolute top-0 w-full z-50 bg-black'>
-          <div className='flex flex-row justify-between items-center px-12 py-2 border-b space-x-2 uppercase tracking-wider text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
+          <div className='flex flex-row justify-between items-center px-8 py-2 border-b space-x-2 uppercase tracking-wider text-2xl md:text-3xl lg:text-4xl xl:text-5xl'>
             <span></span>
             <Link href={'/'} className='text-white hover:text-primary transition-all duration-200'>
               <span className=''>Anthony </span>
@@ -18,17 +18,22 @@ const Home = () => {
             </Link>
             <span></span>
           </div>
-          <ul className="flex flex-row space-x-8 justify-center items-center px-4 py-2 md:text-lg text-white">
-            <li>
-              <NavLink href="/about">About</NavLink>
-            </li>
-            <li>
-              <NavLink href="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink href="/resume">Resume</NavLink>
-            </li>
-          </ul>
+          <div className='lg:hidden text-xs uppercase tracking-wider text-center py-2 text-white'>Made with love by <Link href={'https://spiderfunction.com'} className='text-primary'>Spider Function</Link></div>
+          <div className='flex flex-row justify-between items-center w-full px-8'>
+            <div className='hidden lg:block w-[33%]'></div>
+            <ul className="w-full lg:w-[33%] flex flex-row space-x-8 justify-center items-center px-4 py-2 md:text-lg text-white">
+              <li>
+                <NavLink href="/about">About</NavLink>
+              </li>
+              <li>
+                <NavLink href="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink href="/resume">Resume</NavLink>
+              </li>
+            </ul>
+            <div className='hidden lg:block w-[33%] text-sm uppercase tracking-wider text-right text-white'>Made with love by <Link href={'https://spiderfunction.com'} className='text-primary'>Spider Function</Link></div>
+          </div>
         </nav>
         <ThunderEffect />
         <div className='absolute bottom-48 left-8 hidden lg:block'>
@@ -37,7 +42,7 @@ const Home = () => {
             <TypeWriter />  
           </span>
         </div>
-        <div className='absolute top-30 left-8 block lg:hidden'>
+        <div className='absolute top-36 left-8 block lg:hidden'>
           <span className='text-xl md:text-2xl lg:text-3xl xl:text-4xl text-white'>Hi, I'm <span className='text-primary'>Anthony </span>Philip</span>
           <span className='text-xl md:text-2xl lg:text-3xl xl:text-4xl'>
             <TypeWriter />  
